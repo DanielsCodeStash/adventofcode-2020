4932e02c193d3a0fd8fedda6154e38fc4b139a9b
Bredberg <DBREDBER@volvocars.com>
2020-12-04 10:35:31 +0100

    day 4 solved

--- src/main/kotlin/day4/part1.kt
+++ src/main/kotlin/day4/part1.kt
@@ -2,9 +2,43 @@ package day4
 
 import util.readInput
 
+class Passport(var data: MutableMap<String, String> = mutableMapOf()) {
+    override fun toString(): String {
+        return data.entries.toString()
+    }
+}
+
 fun main() {
 
     val input = readInput(4)
 
+    val passports = mutableListOf<Passport>()
+    var activePassport = Passport()
+
+    for((i, row) in input.withIndex()) {
+
+        if(i == input.size-1 || row.isBlank()) {
+            passports.add(activePassport)
+            activePassport = Passport()
+            continue
+        }
+
+        row.split(" ").forEach { dataEntry ->
+            val data = dataEntry.split(":")
+            activePassport.data[data[0]] = data[1]
+        }
+    }
+
+    val validPassports = passports.filter { passportIsValid(it) }
+
+    println(validPassports)
+
+    println("Total parsed passports: ${passports.size}")
+    println("Number of valid passports: ${validPassports.count()}")
+}
+
+private fun passportIsValid(passport: Passport) : Boolean {
+    val requiredFields = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+    return requiredFields.none { !passport.data.containsKey(it) }
 }
 
